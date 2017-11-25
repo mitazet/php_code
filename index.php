9e1c3d017e9f75eb6f0576df3b343150cf5f11ae
mitazet <mitazet@gmail.com>
2017-11-25 10:58:25 +0900

    add message system

--- index.php
+++ index.php
@@ -4,7 +4,30 @@ function func_sec(){
     return;
 }
 
+function func_set_msg($msg){
+    $filename = '/home/pi/work/msg.txt';
+    file_put_contents($filename, $msg);
+    return;
+}
+
+function func_send_msg(){
+    exec("python3 /home/pi/python_code/slack_msg.py");
+    $filename = '/home/pi/work/msg.txt';
+    file_put_contents($filename, '');
+
+    return;
+}
+
+if($_POST['text'] == "set_msg"):
+    func_set_msg($_POST['msg']);
+endif;
+
+if($_POST['text'] == "send_msg"):
+    func_send_msg();
+endif;
+
 if($_POST['text'] == "security"):
     func_sec();
 endif;
 ?>
+
